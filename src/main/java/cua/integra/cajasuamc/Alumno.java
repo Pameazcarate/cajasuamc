173f32cf5367d3355c46d09e238bfaedb08fbd07
Pameazcarate <pamela.azcarate@cua.uam.mx>
2025-04-04 09:48:43 -0600

    Modificacion en pom

---
+++ src/main/java/cua/integra/cajasuamc/Alumno.java
@@ -0,0 +1,26 @@
+package cua.integra.cajasuamc;
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import lombok.Setter;
+
+@Entity
+@Table(name = "alumnos")
+@Setter
+public class Alumno {
+    @Id
+    private long id;
+    @Column(nullable = false, unique = true)
+    private String matricula;
+    @Column(nullable = false)
+    private String nombre;
+    @Column(nullable = false)
+    private String apellido;
+    @Column(nullable = false)
+    private String telefono;
+    @Column(nullable = false, name = "telefono_contacto")
+    private String telefonoContacto;
+    @Column(nullable = false)
+    private String password;
+}
