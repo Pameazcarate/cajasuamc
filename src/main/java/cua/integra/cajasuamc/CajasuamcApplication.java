7d53c4e22a66e28118c65688896dfe7f1f834095
Pameazcarate <pamela.azcarate@cua.uam.mx>
2025-04-04 08:34:17 -0600

    Primer commit

---
+++ src/main/java/cua/integra/cajasuamc/CajasuamcApplication.java
@@ -0,0 +1,13 @@
+package cua.integra.cajasuamc;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CajasuamcApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CajasuamcApplication.class, args);
+	}
+
+}
---
+++ src/test/java/cua/integra/cajasuamc/CajasuamcApplicationTests.java
@@ -0,0 +1,13 @@
+package cua.integra.cajasuamc;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CajasuamcApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
