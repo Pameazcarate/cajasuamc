d304f0c07f01a7dc9e6577c20833a9712fb9c6c4
Pameazcarate <pamela.azcarate@cua.uam.mx>
2025-04-04 10:02:13 -0600

    Clase de prueba

--- src/main/java/cua/integra/cajasuamc/Prueba.java
+++ src/main/java/cua/integra/cajasuamc/Prueba.java
@@ -1,7 +1,12 @@
 package cua.integra.cajasuamc;
 
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
 public class Prueba {
-    public static void main(String[] args) {
-        System.out.println("Modificación en el repositorio.");
+    @GetMapping("/v1")
+    public String v1() {
+        return "Pruebas desde rama development.";
     }
 }
