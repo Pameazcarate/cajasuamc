0f0dda911005216dd2db0056b74fde5943b6c8eb
Pameazcarate <pamela.azcarate@cua.uam.mx>
2025-04-29 22:08:47 -0600

    Cambio Alumno

--- src/main/java/cua/integra/cajasuamc/entity/Alumno.java
+++ src/main/java/cua/integra/cajasuamc/entity/Alumno.java
@@ -1,30 +1,4 @@
 package cua.integra.cajasuamc.entity;
-import jakarta.persistence.Column;
-import jakarta.persistence.Entity;
-import jakarta.persistence.Id;
-import jakarta.persistence.Table;
-import lombok.Setter;
-
-@Entity
-@Table(name = "alumnos")
-@Setter
-public class Alumno {
-    @Id
-    private long id;
-    @Column(nullable = false, unique = true)
-    private String matricula;
-    @Column(nullable = false)
-    private String nombre;
-    @Column(nullable = false)
-    private String apellido;
-    @Column(nullable = false)
-    private String telefono;
-    @Column(nullable = false, name = "telefono_contacto")
-    private String telefonoContacto;
-    @Column(nullable = false)
-    private String password;
-=======
-
 import jakarta.persistence.*;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
