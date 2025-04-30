917155a648088a280b15004efed4dc650f8a1d6a
Pameazcarate <pamela.azcarate@cua.uam.mx>
2025-04-29 21:16:01 -0600

    Alumno y Cajas modificacion

--- src/main/java/cua/integra/cajasuamc/entity/Pago.java
+++ src/main/java/cua/integra/cajasuamc/entity/Pago.java
@@ -1,28 +1,4 @@
 package cua.integra.cajasuamc.entity;
 
-import jakarta.persistence.*;
-import lombok.Getter;
-import lombok.Setter;
-
-import java.util.Date;
-import java.util.List;
-
-    @Entity
-    @Table(name = "pago")
-    @Getter
-    @Setter
-    public class Pago {
-        @Id
-        @GeneratedValue(strategy = GenerationType.IDENTITY)
-        private long id;
-        @Column(nullable = false, unique = true)
-        private Date fechaPago;
-        @Column(nullable = false)
-        private String monto;
-        @ManyToOne
-        @JoinColumn(name = "alumno_id")
-        private Alumno alumno;
-        @ManyToOne
-        @JoinColumn(name = "caja_id")
-        private Caja caja;
-        }
+public class Pago {
+}
