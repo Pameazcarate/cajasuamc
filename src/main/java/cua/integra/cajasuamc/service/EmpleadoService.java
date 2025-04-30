72fb3cb37bbdc7e7c0a1ae69a49b76f5d5cc8fce
JacqueBaPa <167914947+JacquelineBaPa@users.noreply.github.com>
2025-04-29 23:22:36 -0600

    Update EmpleadoService.java

--- src/main/java/cua/integra/cajasuamc/service/EmpleadoService.java
+++ src/main/java/cua/integra/cajasuamc/service/EmpleadoService.java
@@ -1,12 +1,15 @@
 package cua.integra.cajasuamc.service;
 
 import cua.integra.cajasuamc.dto.EmpleadoDTO;
+import org.springframework.stereotype.Service;
 
 import java.util.List;
 
+@Service
 public interface EmpleadoService {
     EmpleadoDTO createEmpleado(EmpleadoDTO empleadoDTO);
     List<EmpleadoDTO> getAllEmpleados();
     EmpleadoDTO getEmpleadoBynumeroEmpleado(long numeroEmpleado);
     EmpleadoDTO updateEmpleado(long numeroEmpleado, EmpleadoDTO empleadoDTO);
 }
+
